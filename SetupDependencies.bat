33c435eb3b464f6e17d85de5ddb0067648122974
Z Guan <AirGuanZ@gmail.com>
2023-12-29 22:44:39 +0800

    Always use llvm compiled with release config

--- SetupDependencies.bat
+++ SetupDependencies.bat
@@ -15,16 +15,16 @@ if exist "./llvm-project-16.0.6.src/" rmdir /s /q "./llvm-project-16.0.6.src"
 set "llvm_root=%CD%"
 set "llvm_root=%llvm_root:\=/%"
 
-if exist "./build_debug/" rmdir /s /q "./build_debug"
-if exist "./install_debug/" rmdir /s /q "./install_debug"
-mkdir build_debug
-mkdir install_debug
-cd build_debug
-cmake ..\llvm-project-16.0.6.src\llvm\ -DLLVM_ENABLE_PROJECTS='clang' -DCMAKE_INSTALL_PREFIX='%llvm_root%/install_debug' -DCMAKE_BUILD_TYPE=Debug -DLLVM_USE_CRT_DEBUG=MTd
-cmake --build . --config=Debug --parallel 16
-cmake --install . --config=Debug
-cd ..
-if exist "./build_debug/" rmdir /s /q "./build_debug"
+::if exist "./build_debug/" rmdir /s /q "./build_debug"
+::if exist "./install_debug/" rmdir /s /q "./install_debug"
+::mkdir build_debug
+::mkdir install_debug
+::cd build_debug
+::cmake ..\llvm-project-16.0.6.src\llvm\ -DLLVM_ENABLE_PROJECTS='clang' -DCMAKE_INSTALL_PREFIX='%llvm_root%/install_debug' -DCMAKE_BUILD_TYPE=Debug -DLLVM_USE_CRT_DEBUG=MTd
+::cmake --build . --config=Debug --parallel 16
+::cmake --install . --config=Debug
+::cd ..
+::if exist "./build_debug/" rmdir /s /q "./build_debug"
 
 if exist "./build_release/" rmdir /s /q "./build_release"
 if exist "./install_release/" rmdir /s /q "./install_release"
