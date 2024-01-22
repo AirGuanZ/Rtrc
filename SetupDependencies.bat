fb71c53692f3f51919171c85c19741a3d0de89f6
airguanz <airguanz@gmail.com>
2024-01-22 11:21:48 +0800

    Add eigen as local compressed dependencies

--- SetupDependencies.bat
+++ SetupDependencies.bat
@@ -4,3 +4,10 @@ cd ./External/
 if exist "./oneapi-tbb-2021.11.0/" rmdir /s /q "./oneapi-tbb-2021.11.0/"
 "C:/Program Files/7-Zip/7z.exe" x -y oneapi-tbb-2021.11.0.7z
 cd ../
+
+:: ====================================== eigen ======================================
+
+cd ./External/
+if exist "./eigen-3.4.0/" rmdir /s /q "./eigen-3.4.0/"
+"C:/Program Files/7-Zip/7z.exe" x -y eigen-3.4.0.7z
+cd ../
