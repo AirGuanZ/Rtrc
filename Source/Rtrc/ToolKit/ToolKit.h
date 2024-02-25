1c4c9292fd408bdb0cdec18a1cc13100d24e8c1f
Z Guan <AirGuanZ@gmail.com>
2024-02-25 16:05:06 +0800

    Update ToolKit.h

--- Source/Rtrc/ToolKit/ToolKit.h
+++ Source/Rtrc/ToolKit/ToolKit.h
@@ -10,6 +10,7 @@
 #include <Rtrc/ToolKit/ImGui/ImGuiInstance.h>
 #include <Rtrc/ToolKit/IndirectArg/PrepareThreadGroupCountForIndirectDispatch.h>
 #include <Rtrc/ToolKit/Mesh/UniformGrid.h>
+#include <Rtrc/ToolKit/OT2D/OT2D.h>
 #include <Rtrc/ToolKit/PcgState/PcgStateTexture.h>
 #include <Rtrc/ToolKit/Resource/BindlessResourceManager.h>
 #include <Rtrc/ToolKit/Resource/ResourceManager.h>
