851b8a676e63281b9731b68cb0a8e828759fc602
Z Guan <AirGuanZ@gmail.com>
2024-07-21 10:36:35 +0800

    Remove write access in aliasing barrier

--- Source/Rtrc/Graphics/RenderGraph/Compiler.cpp
+++ Source/Rtrc/Graphics/RenderGraph/Compiler.cpp
@@ -957,7 +957,7 @@ void RGCompiler::GenerateBarriers(const RGExecutableResources &resources)
                 {
                     auto &prevFinalState = resources.indexToBuffer[prev].finalState;
                     lastState.stages |= prevFinalState.stages;
-                    lastState.accesses |= prevFinalState.accesses;
+                    lastState.accesses |= prevFinalState.accesses & ~RHI::ResourceAccessWriteMask;
                 }
                 else
                 {
@@ -966,7 +966,7 @@ void RGCompiler::GenerateBarriers(const RGExecutableResources &resources)
                         if(prevFinalState)
                         {
                             lastState.stages |= prevFinalState->stages;
-                            lastState.accesses |= prevFinalState->accesses;
+                            lastState.accesses |= prevFinalState->accesses & ~RHI::ResourceAccessWriteMask;
                         }
                     }
                 }
@@ -1059,7 +1059,7 @@ void RGCompiler::GenerateBarriers(const RGExecutableResources &resources)
                 {
                     auto &prevFinalState = resources.indexToBuffer[prev].finalState;
                     lastState.stages |= prevFinalState.stages;
-                    lastState.accesses |= prevFinalState.accesses;
+                    lastState.accesses |= prevFinalState.accesses & ~RHI::ResourceAccessWriteMask;
                 }
                 else
                 {
@@ -1068,7 +1068,7 @@ void RGCompiler::GenerateBarriers(const RGExecutableResources &resources)
                         if(prevFinalState)
                         {
                             lastState.stages |= prevFinalState->stages;
-                            lastState.accesses |= prevFinalState->accesses;
+                            lastState.accesses |= prevFinalState->accesses & ~RHI::ResourceAccessWriteMask;
                         }
                     }
                 }
