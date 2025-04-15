2cf5a0137247e52cfa7e5e2768f5f9ca87145c9d
Z Guan <AirGuanZ@gmail.com>
2025-04-15 22:12:04 +0800

    Add various distance functions

--- Source/Rtrc/Geometry/DistanceField.h
+++ Source/Rtrc/Geometry/DistanceField.h
@@ -35,6 +35,48 @@ namespace SDF
         return std::sqrt(Rtrc::Dot(nor, pa) * Rtrc::Dot(nor, pa) / Rtrc::LengthSquare(nor));
     }
 
+    template<typename T>
+    T UDLine(const Vector3<T> &p, const Vector3<T> &a, const Vector3<T> &b)
+    {
+        const Vector3<T> pa = p - a, ba = b - a;
+        const T h = Rtrc::Clamp(Dot(pa, ba) / Dot(ba, ba), 0.0, 1.0);
+        return Length(pa - ba * h);
+    }
+
+    // c.x: sin(angle), c.y: cos(angle), h: height
+    // corner is put at the origin and height line points to -y
+    template<typename T>
+    T SDCone(const Vector3<T> &p, const Vector2<T> &c, double h)
+    {
+        const Vector2<T> q = h * Vector2<T>(c.x / c.y, -1.0);
+        const Vector2<T> w = Vector2<T>(Rtrc::Length(Vector2<T>(p.x, p.z)), p.y);
+        const Vector2<T> a = w - q * Rtrc::Clamp<T>(Rtrc::Dot(w, q) / Rtrc::Dot(q, q), 0, 1);
+        const Vector2<T> b = w - q * Vector2<T>(Rtrc::Clamp<T>(w.x / q.x, 0, 1), 1);
+        const T k = SDF::Sign(q.y);
+        const T d = (std::min)(Rtrc::Dot(a, a), Rtrc::Dot(b, b));
+        const T s = (std::max)(k * (w.x * q.y - w.y * q.x), k * (w.y - q.y));
+        return std::sqrt(d) * SDF::Sign(s);
+    }
+
+    // Similar to the previous one except height is infinite
+    template<typename T>
+    T SDCone(const Vector3<T> &p, const Vector2<T> &c)
+    {
+        const Vector2<T> q = Vector2<T>(Rtrc::Length(Vector2<T>(p.x, p.z)), -p.y);
+        const T  d = Rtrc::Length(q - c * (std::max<T>)(Rtrc::Dot(q, c), 0));
+        return d * ((q.x * c.y - q.y * c.x < T(0)) ? T(-1.0) : T(1.0));
+    }
+
+    // c is the sin/cos of the angle
+    template<typename T>
+    T SDSolidAngle(const Vector3<T> p, const Vector2<T> &c, T radius)
+    {
+        const Vector2<T> q = Vector2<T>(Rtrc::Length(Vector2<T>(p.x, p.z)), p.y);
+        const T l = Rtrc::Length(q) - radius;
+        const T m = Rtrc::Length(q - c * Rtrc::Clamp<T>(Rtrc::Dot(q, c), 0, radius));
+        return (std::max)(l, m * SDF::Sign(c.y * q.x - c.x * q.y));
+    }
+
 } // namespace SDF
 
 RTRC_GEO_END
